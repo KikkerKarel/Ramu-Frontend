b8db821d3abcc74642632b696679d673991890eb
KikkerKarel <kishangaming@gmail.com>
2023-03-23 18:22:43 +0100

    updates to home component

--- src/components/molecules/Container/HomeContainer/index.jsx
+++ src/components/molecules/Container/HomeContainer/index.jsx
@@ -1,30 +1,32 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-import React, {Component} from "react";
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container, Row, Col, Button, Image, Stack } from 'react-bootstrap';
 import './index.css';
+import chanmina from '../../../../images/Chanmina-for-Rolling-Stone-Japan-September-2022-documents-1.jpeg';
+import chanmina2 from '../../../../images/ChanminaHeader.png';
 
 
 class HomeContainer extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.handleClick = this.handleClick.bind(this);
     }
 
-    async handleClick(){
+    async handleClick() {
         const name = "Dreamcatcher";
         const token = Cookies.get("SpotifyToken");
         const tokenHash = btoa(token);
         console.log(tokenHash);
-        await axios.get("/ramu/spotify/api/search/artist", { params: {name: name, tokenHash: tokenHash}}).then(response => {
+        await axios.get("/ramu/spotify/api/search/artist", { params: { name: name, tokenHash: tokenHash } }).then(response => {
             console.log(response.data);
         });
     };
 
-    render () {
-        return <Container className="home-container" fluid>
-            <Row className="home-container-row">
+    render() {
+        return <Container fluid>
+            {/* <Row className="home-container-row">
                 <Col className="home-container-row-col">
                     <header>This weeks</header>
                 </Col>
@@ -34,8 +36,29 @@ class HomeContainer extends Component {
                     <div className="numberOne-div">
                         <header>#1</header>
                         <Button onClick={this.handleClick}>Get Artist From Database</Button>
+                        <Image src={chanmina} style={{height: '60%'}} />
                     </div>
                 </Col>
+            </Row> */}
+            {/* <Row  className="home-row-col layer parallax" style={{ backgroundImage: `url(${chanmina})`, backgroundRepeat: "no-repeat", height: '100%', backgroundPosition: 'center'}}>
+            </Row> */}
+            {/* <Row style={{backgroundColor: 'red'}}>
+                <div className="center-image-div layer parallax" style={{ backgroundImage: `url(${chanmina})`, backgroundRepeat: "no-repeat", backgroundPosition: 'center'}}>
+                    <text>test</text>
+                </div>
+            </Row>
+            <Row style={{backgroundColor: 'blue'}}>
+                
+            </Row> */}
+            <Row>
+                <div className="image-div-container" style={{ backgroundImage: `url(${chanmina2})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}></div>
+                <div className="image-div">
+                    <Image src={chanmina} style={{ height: '50%', marginTop: '5%' }} />
+                    <text>test</text>
+                </div>
+            </Row>
+            <Row>
+
             </Row>
         </Container>
     }
--- src/components/organisms/NavBar/index.jsx
+++ src/components/organisms/NavBar/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Container, Row, Col, Form } from 'react-bootstrap';
 import "./index.css";
 import { Navbar } from "react-bootstrap";
@@ -13,7 +13,7 @@ import SpotifyIcon from '../../atoms/Icons/Spotify';
 
 class NavBarComponent extends Component {
 
-    state ={
+    state = {
         open: false,
         canvas: false,
         redirect: false,
@@ -26,7 +26,7 @@ class NavBarComponent extends Component {
         this.handleShowCanvas = this.handleShowCanvas.bind(this);
     }
 
-    handleOpen (event) {
+    handleOpen(event) {
         if (event) {
             this.setState({ open: true });
         } else {
@@ -34,57 +34,55 @@ class NavBarComponent extends Component {
         }
     }
 
-    handleShowCanvas (event) {
+    handleShowCanvas(event) {
         if (event) {
             this.setState({ canvas: true });
         } else {
-            this.setState( {canvas: false });
+            this.setState({ canvas: false });
         }
     }
 
-    handleClose (event) {
+    handleClose(event) {
         if (event) {
             this.setState({ show: false });
         }
     }
 
     render() {
-        return <Navbar className="navBar" bg="dark" variant="dark">
-            <Container fluid className="navbar-container">
-                <Row className="navbar-container-row">
-                    <Col className="navbar-container-row-col" sm={2}>
-                        <Navbar.Brand href="/">
-                            <Logo />
-                        </Navbar.Brand>
-                    </Col>
-                    <Col className="navbar-container-row-col">
-                        <Container className="navigate-container" fluid>
-                            <Row>
-                                <Col id="col-1"><HomeIcon /></Col>
-                                <Col><NoteList open={this.handleOpen} /></Col>
-                                <Col id="col-3"><PersonalPage /></Col>
-                            </Row>
-                        </Container>
-                    </Col>
-                    <Col className="navbar-container-row-col" sm={2}>
-                        <SpotifyIcon />
-                        <SearchIcon show={this.handleShowCanvas} />
-                    </Col>
-                </Row>
-            </Container>
+        return <Navbar className="navBar" variant="dark">
+            <Row className="navbar-row">
+                <Col className="navbar-container-row-col" sm={2}>
+                    <Navbar.Brand href="/">
+                        <Logo />
+                    </Navbar.Brand>
+                </Col>
+                <Col className="navbar-container-row-col">
+                    <Container className="navigate-container" fluid>
+                        <Row>
+                            <Col id="col-1"><HomeIcon /></Col>
+                            <Col><NoteList open={this.handleOpen} /></Col>
+                            <Col id="col-3"><PersonalPage /></Col>
+                        </Row>
+                    </Container>
+                </Col>
+                <Col className="navbar-container-row-col" sm={2}>
+                    <SpotifyIcon />
+                    <SearchIcon show={this.handleShowCanvas} />
+                </Col>
+            </Row>
 
-            <MusicList 
+            <MusicList
                 show={this.state.open}
                 onHide={this.handleOpen}
             />
 
-            <SearchOffCanvas 
+            <SearchOffCanvas
                 show={this.state.canvas}
                 onHide={this.handleShowCanvas}
                 placement="top"
                 name="top"
             />
-            
+
         </Navbar>
     }
 }
