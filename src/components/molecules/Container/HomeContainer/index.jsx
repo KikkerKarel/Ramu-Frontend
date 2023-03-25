45ee09c70ba211dc3c4c56a922c03fd85bcaf953
KikkerKarel <kishangaming@gmail.com>
2023-03-25 12:21:04 +0100

    new home page layout phase 1

--- src/components/molecules/Container/HomeContainer/index.jsx
+++ src/components/molecules/Container/HomeContainer/index.jsx
@@ -1,10 +1,9 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 import React, { Component } from "react";
-import { Container, Row, Col, Button, Image, Stack } from 'react-bootstrap';
+import { Container, Row, Image } from 'react-bootstrap';
 import './index.css';
 import chanmina from '../../../../images/Chanmina-for-Rolling-Stone-Japan-September-2022-documents-1.jpeg';
-import chanmina2 from '../../../../images/ChanminaHeader.png';
 
 
 class HomeContainer extends Component {
@@ -26,42 +25,33 @@ class HomeContainer extends Component {
 
     render() {
         return <Container fluid>
-            {/* <Row className="home-container-row">
-                <Col className="home-container-row-col">
-                    <header>This weeks</header>
-                </Col>
-            </Row>
-            <Row className="home-container-row-2">
-                <Col>
-                    <div className="numberOne-div">
-                        <header>#1</header>
-                        <Button onClick={this.handleClick}>Get Artist From Database</Button>
-                        <Image src={chanmina} style={{height: '60%'}} />
-                    </div>
-                </Col>
-            </Row> */}
-            {/* <Row  className="home-row-col layer parallax" style={{ backgroundImage: `url(${chanmina})`, backgroundRepeat: "no-repeat", height: '100%', backgroundPosition: 'center'}}>
-            </Row> */}
-            {/* <Row style={{backgroundColor: 'red'}}>
-                <div className="center-image-div layer parallax" style={{ backgroundImage: `url(${chanmina})`, backgroundRepeat: "no-repeat", backgroundPosition: 'center'}}>
-                    <text>test</text>
-                </div>
-            </Row>
-            <Row style={{backgroundColor: 'blue'}}>
-                
-            </Row> */}
             <Row>
-                <div style={{ position: 'absolute', height: '60%', overflow: 'hidden'}}>
-                    <div className="image-div-container" style={{ backgroundImage: `url(${chanmina})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}></div>
-                </div>
-                
-                <div className="image-div">
-                    <Image src={chanmina} style={{ height: '100%' }} />
+                <div className="home-container">
+                    <div className="background-div-container" style={{ backgroundImage: `url(${chanmina})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+                        <div className="backgroundFilter-div"></div>
+                        {/* <div className="background-div" style={{ backgroundImage: `url(${chanmina})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}></div> */}
+                    </div>
+
+                    <div>
+                        <div className="image-div">
+                            <Image src={chanmina} style={{ height: '100%' }} />
+                        </div>
+
+                        <div className="home-info-div">
+                            <div className="video-div" id="info-div">
+                                <iframe width="100%" height="225" src="https://www.youtube-nocookie.com/embed/qw7oS1FBHyI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                            <div className="main-info-div" id="info-div">
+                                <h1 id="center">Chanmina</h1>
+                                <text>Chanmina is a Japanese-Korean singer/songwriter. She is most known for her hit singles Doctor and Never Grow Up.</text>
+                            </div>
+                            <div className="video-div" id="info-div">
+                                <iframe width="100%" height="225" src="https://www.youtube-nocookie.com/embed/xlX8EzRdMl8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div id="test">
-                    <text>test</text>
-                </div>
             </Row>
             <Row>
 
